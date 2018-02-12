1cac096708788eba79e0be1efcd4c07b753e239f
Yuri Burger <yuriburger@outlook.com>
2018-02-12 13:16:28 +0100

    PolicyServer setup

---
+++ PolicyServer/Controllers/PoliciesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace policyserver.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class PoliciesController : Controller
+    {
+        // GET api/policies
+        [HttpGet()]
+        public JsonResult Get()
+        {
+            var roles = User.FindAll("role");
+            return new JsonResult(from r in roles select new { r.Type, r.Value });
+        }
+    }
+}
